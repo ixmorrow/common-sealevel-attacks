9e02bef4ddaf6abbc0a69abc9d10584270a5be1e
john <johnzhengyuliu@gmail.com>
2022-10-26 15:16:15 -0500

    signer, owner, data validation

--- test/programs/test/src/lib.rs
+++ test/programs/test/src/lib.rs
@@ -7,9 +7,36 @@ pub mod test {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let data = &mut ctx.accounts.data_account;
+        data.user = ctx.accounts.payer.key();
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>) -> Result<()> {
+        let data = &mut ctx.accounts.data_account;
+        data.user = ctx.accounts.payer.key();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = payer, space = 8 + 32)]
+    pub data_account: Account<'info, Data>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut, constraint = payer.key == &data_account.user)]
+    pub data_account: Account<'info, Data>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+}
+
+#[account]
+pub struct Data {
+    pub user: Pubkey,
+}
